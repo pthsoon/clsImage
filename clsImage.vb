8f6b389fb841f9a890c09a3b492334b6b041579d
pthsoon <pthsoon@gmail.com>
2020-08-10 12:14:57 +0800

    Image save

---
+++ clsImage.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Drawing.Imaging
+
+Public Class clsImage
+    Private Image As Image
+
+    Public Function LoadImage(ByVal SourceFile As String) As Image
+
+        Using bitmap = New Bitmap(SourceFile)
+            image = New Bitmap(bitmap)
+
+        End Using
+        Return Image
+    End Function
+
+
+    Public Sub SaveFileJPG(ByVal SourceFile As String, ByVal DestinationFile As String)
+        Dim Image As Image = LoadImage(SourceFile)
+        Image.Save(DestinationFile, ImageFormat.Jpeg)
+
+    End Sub
+
+    Private _disposed As Boolean
+    Protected Overridable Sub Dispose(ByVal disposing As Boolean)
+        If _disposed Then Return
+
+        If disposing Then
+
+
+            If image IsNot Nothing Then
+                image.Dispose()
+            End If
+        End If
+
+        _disposed = True
+    End Sub
+    Public Sub Dispose()
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
